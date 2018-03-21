6ebd30fe764a811428aa219ab15e6a03813d4161
Joy <447016299@qq.com>
2018-03-21 17:00:13 +0800

    day01end

--- pages/release/release.js
+++ pages/release/release.js
@@ -1,78 +1,66 @@
+// pages/release/release.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      '../../img/index/picture/Fzu00.jpg',
-      '../../img/index/picture/Fzu01.jpg',
-      '../../img/index/picture/Fzu02.jpg',
-      '../../img/index/picture/Fzu03.jpg'
-    ],
-    iconUrls:[
-      "../../img/index/icon/Ershou.png",
-      "../../img/index/icon/Jianzhi.png",
-      "../../img/index/icon/Shiwu.png",
-      "../../img/index/icon/Xueyuan.png",
-      "../../img/index/icon/Jiuye.png",
-      "../../img/index/icon/Pinche.png",
-    ]
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+  
   }
 })
